1a092b7121ed7b9c37cea568a624e72634b4610e
PedroBrandao05 <pedro-brandao2012@hotmail.com>
2023-07-03 14:45:44 -0300

    infra: changed to postgresql

--- src/infra/db/driver.ts
+++ src/infra/db/driver.ts
@@ -2,13 +2,15 @@ import { injectable } from "inversify";
 import 'reflect-metadata'
 import { IDatabaseDriver } from "../../application/contracts/databaseDriver";
 import connection from ".";
+import { disconnect } from ".";
 
 @injectable()
 export default class DatabaseDriver implements IDatabaseDriver {
     
     async run(query: string): Promise<any> {
-        connection.query(query, function(err, results, fields){
-            return results
-        })
+        const database = await connection()
+        const results = await database.query(query)
+        await disconnect(database)
+        return results
     }
 }
--- src/infra/db/index.ts
+++ src/infra/db/index.ts
@@ -1,18 +1,25 @@
-import mysql from 'mysql2'
 import dotenv from 'dotenv'
+import pgPromise from 'pg-promise'
 
-dotenv.config()
-
-const port = Number(process.env.DB_PORT)
-
-const connection = mysql.createConnection({
+const config = {
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
-    port,
+    port: Number(process.env.DB_PORT),
     database: process.env.DB_NAME
-})
+}
 
-connection.connect()
+const connection = async () => {
+  const postgre = pgPromise({})
+  const db = postgre(config)
+  db.connect().then(() => console.log('Connected to database')).catch((err) => {console.log(err)})
+  return db;
+}
+
+export const disconnect = async (
+    db: pgPromise.IDatabase<any, any>
+  ): Promise<void> => {
+    await db.$pool.end()
+  }
 
 export default connection
